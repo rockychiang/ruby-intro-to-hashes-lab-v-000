d77c304b7c9b2042c65e270f8473b60fbd7fd5ee
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-12 03:20:19 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly
 	  :railroads => {
 	    pieces: 4,
 	    names: {
-	      reading_railroad: {},
+	      reading_railroad: {mortgage_value => "$100"},
 	      pennsylvania_railroad: {},
 	      b_and_o_railroad: {},
 	      shortline: {}
