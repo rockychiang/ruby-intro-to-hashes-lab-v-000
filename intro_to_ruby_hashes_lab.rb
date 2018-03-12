4b59e5f92d0e652591c532469bf5093d17ca3590
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-12 03:16:46 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,12 +8,16 @@ end
 
 def monopoly
 	monopoly = {
-	  :railroads => {pieces: 4, names: {}, rent_in_dollars: {
-	    one_piece_owned: 25,
-	    two_piece_owned: 50, 
-	    three_piece_owned: 100, 
-	    four_piece_owned: 200
-	  }}
+	  :railroads => {
+	    pieces: 4,
+	    names: {},
+	    rent_in_dollars: {
+  	    one_piece_owned: 25,
+  	    two_piece_owned: 50, 
+  	    three_piece_owned: 100, 
+  	    four_piece_owned: 200
+  	  }
+	  }
 	}
 
 end
