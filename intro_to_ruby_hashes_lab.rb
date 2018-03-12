d486f52e58bf4154ebc513c2f5406d0f84eff5a4
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-12 03:25:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly
 	      b_and_o_railroad: {"mortgage_value" => "$400"},
 	      shortline: {"mortgage_value" => "$800"}
 	    },
-	    rent_in_dollars: {
+	    :rent_in_dollars => {
   	    one_piece_owned: 25,
   	    two_piece_owned: 50, 
   	    three_piece_owned: 100, 
